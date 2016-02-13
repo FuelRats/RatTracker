80373f163228f234766cf028e651fe5505953c3b
Kenneth Aalberg <kenneaal@periscopegames.com>
2016-02-14 00:08:07 +0100

    Add PlayerInfo class, to replace various mainwindow vars.

---
+++ RatTracker-WPF/RatTracker-WPF/Models/App/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatTracker_WPF.Models.App
+{
+    class PlayerInfo
+    {
+        public string CurrentSystem { get; set; }
+        public bool OnDuty { get; set; }
+        public float JumpRange { get; set; }
+        public float SuperCruise { get; set; }
+    }
+}
