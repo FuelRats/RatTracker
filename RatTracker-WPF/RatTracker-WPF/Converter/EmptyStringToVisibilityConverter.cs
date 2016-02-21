ed4b7e1c06fb1ec0ac747ffd5de8b0cb9bdfd42d
Adanaran <mikeon@t-online.de>
2016-02-21 17:49:48 +0100

    Bind rat2 and rat3 visibilty to ratname.

---
+++ RatTracker-WPF/RatTracker-WPF/Converter/EmptyStringToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace RatTracker_WPF.Converter
+{
+	public class EmptyStringToVisibilityConverter : MarkupExtension, IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var name = value as string;
+			return string.IsNullOrWhiteSpace(name) ? Visibility.Hidden : Visibility.Visible;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return this;
+		}
+	}
+}
