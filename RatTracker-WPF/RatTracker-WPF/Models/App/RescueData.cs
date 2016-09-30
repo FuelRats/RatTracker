cdb45cad59e6257322ad3fb9d86bbb70166fc762
Kenneth Aalberg <kenneaal@periscopegames.com>
2016-09-30 13:40:33 +0200

    Add RescueData for integration to rat progress displays. Test if this commit can trigger a Jira update: Fixes RAT-6.

---
+++ RatTracker-WPF/RatTracker-WPF/Models/App/RescueData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatTracker_WPF.Models.App
+{
+    class RescueData
+    {
+        public string id { get; set; }
+        public string Client { get; set; }
+
+    }
+}
