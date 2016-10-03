baf1830753f1669aab5bb88d0ccbb120df838165
UncleClapton <camjwelt@gmail.com>
2016-10-03 15:55:27 +0000

    add common interface for all cmdr log event types

---
+++ RatTracker-WPF/RatTracker-WPF/Models/CmdrLog/ICmdrLogEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RatTracker_WPF.Models.CmdrLog {
+    /// <summary>
+    /// Provides a common interface for all CmdrLog entries.
+    /// </summary>
+    public interface ICmdrLogEntry {
+        /// <summary>
+        /// Time the event occured
+        /// </summary>
+        DateTime Timestamp { get; set; }
+
+        /// <summary>
+        /// Name of the event as seen in the cmdr log.
+        /// </summary>
+        string Event { get; set; }
+    }
+}
