951fe92c97d0ce9a32f433c37db6933b49abe8fe
UncleClapton <camjwelt@gmail.com>
2016-10-08 00:07:48 +0000

    Scrapped that idea for now, but will return to it again soon.

--- RatTracker-WPF/RatTracker-WPF/Models/EventArgs/NewCmdrLogFileEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using RatTracker_WPF.Models.CmdrLog;
-
-namespace RatTracker_WPF.Models.EventArgs {
-
-    public class NewCmdrLogFileEventArgs : System.EventArgs {
-        public CmdrLogFile logFile { get; set; }
-    }
-
-}
