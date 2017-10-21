3e8987e39c848032db06e92b69fa43f640f741ce
Adanaran <mikeon@t-online.de>
2017-10-21 12:38:47 +0200

    Stuff?

---
+++ RatTracker-WPF/RatTracker-WPF/Bootstrapping/Module.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+using RatTracker_WPF.Api;
+
+namespace RatTracker_WPF.Bootstrapping
+{
+  public class Module : NinjectModule
+  {
+    public override void Load()
+    {
+      Bind<ApiWorker>().ToSelf().InSingletonScope();
+    }
+  }
+}
