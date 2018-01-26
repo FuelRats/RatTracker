9ef7ab0337a8e6e8c4a5e3562ee7831135fffdc4
Adanaran <mikeon@t-online.de>
2018-01-26 19:55:28 +0100

    Use dev.api.fuelrats.com for v2.1 update

--- RatTracker/Properties/Settings.Designer.cs
+++ RatTracker/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace RatTracker.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace RatTracker.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://api.fuelrats.com/")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://dev.api.fuelrats.com/")]
         public string ApiUrl {
             get {
                 return ((string)(this["ApiUrl"]));
@@ -79,7 +79,7 @@ namespace RatTracker.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://fuelrats.com/")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://dev.fuelrats.com/")]
         public string WebsiteUrl {
             get {
                 return ((string)(this["WebsiteUrl"]));
